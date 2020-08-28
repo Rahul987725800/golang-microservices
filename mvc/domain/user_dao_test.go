c2f056a19a75c8241eaf5c56f87f00dc8e227d1a
Rahul Gupta <guptarahul70322@gmail.com>
2020-08-28 15:49:03 +0530

    added assert

--- mvc/domain/user_dao_test.go
+++ mvc/domain/user_dao_test.go
@@ -3,17 +3,17 @@ package domain
 import (
 	"net/http"
 	"testing"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGetUserNoUserFound(t *testing.T) {
-	user, err := GetUser(0)
-	if user != nil {
-		t.Error("We were not expecting user with id 0")
+	user, err := GetUser(1)
+	assert.Nil(t, user, "We were not expecting user with id 0")
+	assert.NotNil(t, err, "we were expecting an error when user id is 0")
+	if err != nil {
+		assert.EqualValues(t, http.StatusNotFound, err.StatusCode)
+		assert.EqualValues(t, "user with id 0 was not found", err.Message)
+		assert.EqualValues(t, "not_found", err.Code)
 	}
-	if err == nil {
-		t.Error("we were expecting an error when user id is 0")
-	}
-	if err != nil && err.StatusCode != http.StatusNotFound {
-		t.Error("we were expecting 404 when user is not found")
-	}
+
 }
