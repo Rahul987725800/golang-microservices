2498bbf9a641e7364d736513274c5a8202d6c2b3
Rahul Gupta <guptarahul70322@gmail.com>
2020-08-28 16:14:55 +0530

    test existing user

--- mvc/domain/user_dao_test.go
+++ mvc/domain/user_dao_test.go
@@ -3,17 +3,27 @@ package domain
 import (
 	"net/http"
 	"testing"
+
 	"github.com/stretchr/testify/assert"
 )
 
 func TestGetUserNoUserFound(t *testing.T) {
-	user, err := GetUser(1)
+	// Initialization:
+	// Execution:
+	user, err := GetUser(0)
+	// Validation:
 	assert.Nil(t, user, "We were not expecting user with id 0")
 	assert.NotNil(t, err, "we were expecting an error when user id is 0")
-	if err != nil {
-		assert.EqualValues(t, http.StatusNotFound, err.StatusCode)
-		assert.EqualValues(t, "user with id 0 was not found", err.Message)
-		assert.EqualValues(t, "not_found", err.Code)
-	}
-
+	assert.EqualValues(t, http.StatusNotFound, err.StatusCode)
+	assert.EqualValues(t, "user with id 0 was not found", err.Message)
+	assert.EqualValues(t, "not_found", err.Code)
+}
+func TestGetUserNoError(t *testing.T) {
+	user, err := GetUser(1)
+	assert.Nil(t, err)
+	assert.NotNil(t, user)
+	assert.EqualValues(t, 1, user.ID)
+	assert.EqualValues(t, "Rahul", user.FirstName)
+	assert.EqualValues(t, "Gupta", user.LastName)
+	assert.EqualValues(t, "guptarahul@gmail.com", user.Email)
 }
